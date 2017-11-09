d39bb16096c92a210a4c1f8f8883e707d6ba5866
Ece KARAÇANTA <ecekaracanta@Ece-iMac.local>
2017-11-09 11:09:16 +0300

    The Rider View Controller tamamlandı.

--- Uber/RiderViewController.swift
+++ Uber/RiderViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import MapKit
+import FirebaseDatabase
+import FirebaseAuth
 
 class RiderViewController: UIViewController, CLLocationManagerDelegate {
     
@@ -15,6 +17,8 @@ class RiderViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var btnOutlet_callUber: UIButton!
     
     var locationManager = CLLocationManager()
+    var userLocation = CLLocationCoordinate2D()
+    var uberHasBeenCalled = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,11 +27,20 @@ class RiderViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
+        
+        if let email = Auth.auth().currentUser?.email {
+            Database.database().reference().child("RideRequest").queryOrdered(byChild: "email").queryEqual(toValue: email).observe(.childAdded, with: { (snapshot) in
+                self.uberHasBeenCalled = true
+                self.btnOutlet_callUber.setTitle("Cancel Uber", for: .normal)
+                Database.database().reference().child("RideRequest").removeAllObservers()
+            })
+        }
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let coord = manager.location?.coordinate {
             let center = CLLocationCoordinate2D(latitude: coord.latitude, longitude: coord.longitude)
+            userLocation = center
             let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
             map_rider.setRegion(region, animated: true)
             map_rider.removeAnnotations(map_rider.annotations)
@@ -39,9 +52,28 @@ class RiderViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     @IBAction func btn_logOut(_ sender: Any) {
+        try? Auth.auth().signOut()
+        navigationController?.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func btn_callAnUber(_ sender: Any) {
+        
+        if let email = Auth.auth().currentUser?.email {
+            if uberHasBeenCalled {
+                uberHasBeenCalled = false
+                btnOutlet_callUber.setTitle("Call an Uber", for: .normal)
+                Database.database().reference().child("RideRequest").queryOrdered(byChild: "email").queryEqual(toValue: email).observe(.childAdded, with: { (snapshot) in
+                    snapshot.ref.removeValue()
+                    Database.database().reference().child("RideRequest").removeAllObservers()
+                })
+                
+            } else {
+                let rideRequestDictionary : [String:Any] = ["email" : email, "lat" : userLocation.latitude, "lon" : userLocation.longitude]
+                Database.database().reference().child("RideRequest").childByAutoId().setValue(rideRequestDictionary )
+                uberHasBeenCalled = true
+                btnOutlet_callUber.setTitle("Cancel Uber", for: .normal)
+                
+            }
+        }
     }
-    
 }
